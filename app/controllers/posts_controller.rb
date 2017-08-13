ffcae139c0345916f87477fd740b94fb94779b31
Darren Burgess <darrentburgess@gmail.com>
2017-08-13 16:20:14 -0400

    add index view for posts and post partial

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,20 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+  end
 
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+  
+  def update
   end
 end
