abe77ba6dca58f4989180edb3694be752ebedf07
Darren Burgess <darrentburgess@gmail.com>
2017-08-22 20:38:53 -0400

    add url and date helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def full_url(url)
+    if url.start_with? 'http://'
+      url
+    else
+      "http://#{url}"
+    end
+  end
+
+  def friendly_date(dt)
+    dt.strftime("%m/%d/%Y %l:%M%P %Z")
+  end
 end
