4490c6eb10328626b4ba9a71995b62f6e0195ede
Darren Burgess <darrentburgess@gmail.com>
2017-08-24 15:47:38 -0400

    move set categories to app controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,11 @@
 class CommentsController < ApplicationController
   before_action :require_user, only: [:create]
+  before_action :set_categories, only: [:create]
 
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build(comment_params)
     @comment.author = User.first # TODO: convert to logged in user
-    @categories = Category.all
 
     if @comment.save
       flash[:notice] = "Your comment was created"
